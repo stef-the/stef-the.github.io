7617b188a3d32c581fb54550369da6196ed3e9f4
stef <stefthedoggo4@gmail.com>
2022-09-06 20:23:06 +0100

    scroll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,6 +18,10 @@
 		'Halløj'
 	];
 
+    let svgFill = '#e5e7eb';
+
+    const helloFont = [];
+
 	setInterval(() => {
 		helloVar = hello[Math.floor(Math.random() * hello.length)];
 	}, 1500);
@@ -25,69 +29,80 @@
 
 <main id="main" class="font-sans bg-gray-200 h-screen overflow-hidden">
 	<div class="grid place-content-center h-screen">
-		<div
-			class="transition text-gray-800 hover:-translate-y-4 hover:bg-gray-800 hover:text-gray-200 grid place-content-center w-96 h-44 cursor-pointer rounded-md"
-			on:click={() => {
-                document.getElementById('main')?.classList.add('h-max')
-                document.getElementById('main')?.classList.remove('h-screen')
-				console.log('overflow turned on');
-			}}
-		>
-			<h1 class="text-6xl cursor-inherit text-inherit pb-2 transition">{helloVar}</h1>
-			<div class="flex justify-center">
-				<svg
-					version="1.1"
-					id="Capa_1"
-					xmlns="http://www.w3.org/2000/svg"
-					xmlns:xlink="http://www.w3.org/1999/xlink"
-					viewBox="0 0 20.633 20.633"
-					style="enable-background:new 0 0 20.633 20.633;"
-					xml:space="preserve"
-					width="40"
-					height="40"
-				>
-					<g>
-						<path
-							fill="#e5e7eb"
-							d="M10.79,15.617l9.648-9.646c0.133-0.131,0.195-0.301,0.195-0.473s-0.062-0.344-0.195-0.473l-0.012-0.012
-                c-0.125-0.127-0.295-0.195-0.472-0.195h-4.682c-0.18,0-0.348,0.068-0.473,0.195l-4.48,4.479l-4.48-4.479
-                C5.711,4.886,5.54,4.818,5.366,4.818H0.684c-0.182,0-0.349,0.068-0.475,0.195L0.196,5.025C0.068,5.148,0,5.322,0,5.498
-                c0,0.176,0.068,0.348,0.196,0.473l9.648,9.646C10.108,15.88,10.53,15.88,10.79,15.617z"
-						/>
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-					</g>
-					<g />
-					<g />
-					<g />
-					<g />
-					<g />
-					<g />
-					<g />
-					<g />
-					<g />
-					<g />
-					<g />
-					<g />
-					<g />
-					<g />
-					<g />
-				</svg>
-			</div>
-		</div>
+        <a href="#two">
+            <div
+                class="transition text-gray-800 hover:-translate-y-6 grid place-content-center w-96 h-44 cursor-pointer rounded-md"
+                on:click={() => {
+                    document.getElementById('main')?.classList.add('h-max');
+                    document.getElementById('main')?.classList.remove('h-screen');
+                    console.log('overflow turned on');
+                }}
+                on:mouseenter={() => {
+                    svgFill = '#1f2937';
+                    document.getElementById('svgContainer')?.classList.add('translate-y-4');
+                }}
+                on:mouseleave={() => {
+                    svgFill = '#e5e7eb';
+                    document.getElementById('svgContainer')?.classList.remove('translate-y-4');
+                }}
+            >
+                <h1 class="text-6xl cursor-inherit text-inherit transition">{helloVar}</h1>
+                <div id="svgContainer" class="transition flex justify-center">
+                    <svg
+                        version="1.1"
+                        id="downArrow"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        viewBox="0 0 20.633 20.633"
+                        style="enable-background:new 0 0 20.633 20.633;"
+                        xml:space="preserve"
+                        width="40"
+                        height="40"
+                    >
+                        <g>
+                            <path
+                                id="downArrowPath"
+                                fill="{svgFill}"
+                                d="M10.79,15.617l9.648-9.646c0.133-0.131,0.195-0.301,0.195-0.473s-0.062-0.344-0.195-0.473l-0.012-0.012
+                    c-0.125-0.127-0.295-0.195-0.472-0.195h-4.682c-0.18,0-0.348,0.068-0.473,0.195l-4.48,4.479l-4.48-4.479
+                    C5.711,4.886,5.54,4.818,5.366,4.818H0.684c-0.182,0-0.349,0.068-0.475,0.195L0.196,5.025C0.068,5.148,0,5.322,0,5.498
+                    c0,0.176,0.068,0.348,0.196,0.473l9.648,9.646C10.108,15.88,10.53,15.88,10.79,15.617z"
+                            />
+                            <g />
+                            <g />
+                            <g />
+                            <g />
+                            <g />
+                            <g />
+                            <g />
+                            <g />
+                            <g />
+                            <g />
+                            <g />
+                            <g />
+                            <g />
+                            <g />
+                            <g />
+                        </g>
+                        <g />
+                        <g />
+                        <g />
+                        <g />
+                        <g />
+                        <g />
+                        <g />
+                        <g />
+                        <g />
+                        <g />
+                        <g />
+                        <g />
+                        <g />
+                        <g />
+                        <g />
+                    </svg>
+                </div>
+            </div>
+        </a>
 	</div>
-	<div class="grid place-content-center h-screen">height</div>
+	<div id="two" class="grid place-content-center h-screen">height</div>
 </main>
