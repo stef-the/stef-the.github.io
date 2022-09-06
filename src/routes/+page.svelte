1eb2aacc1a7a5a0859fa535fb5a23da85a408fe5
stef <stefthedoggo4@gmail.com>
2022-09-06 15:39:16 +0100

    Hello World

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	let tick = 0;
+    let helloVar = 'Hello'
+    const hello = ['Hello', 'Bonjour', 'Hallo', 'Hola', 'Hallå', 'Selam', '你好', 'こんにちは', 'Привет', 'Kumusta', 'Ciao', 'Olá', 'Ahlan', 'Halløj'];
+
+	setInterval(() => {
+		helloVar = hello[Math.floor(Math.random() * hello.length)];;
+	}, 1500);
+</script>
+
+<main class="font-sans bg-gray-200 h-screen">
+    <div class="grid place-content-center h-full">
+        <h1 class="text-6xl">{helloVar}</h1>
+    </div>
+</main>
